69f8e1b0aa0cfb6931453b2a5bfbd737a5bd0851
MattPereira00 <142404518+MattPereira00@users.noreply.github.com>
2026-02-20 09:42:29 +0000

    Refactoring existing files for PEP8 compliance

--- 10-BDSIM/main.py
+++ 10-BDSIM/main.py
@@ -26,29 +26,29 @@ Options.SetNGenerate(10000)
 if __name__ == "__main__":
     start = time.time()
     Builder = Builder(Beam, Options, "10-Model", "90-BDSIMData")
-    # config = OptConfig(
-    #     objectives=["T", "A", "D"],
-    #     constraints = {"T_min": 0.04, "D_max": 0.04},
-    #     bounds={
-    #         "d1": (0.0, 0.25),
-    #         "d2": (0.0, 0.25),
-    #         "d3": (0.0, 0.25),
-    #         "q1_L": (0.02, 0.05),
-    #         "q2_L": (0.02, 0.05),
-    #         "q3_L": (0.02, 0.05),
-    #         "aper1": (0.01, 0.04),
-    #         "aper2": (0.01, 0.04),
-    #         "aper3": (0.01, 0.04),
-    #     },
-    #     n_initial=15,
-    #     n_iter=20,
-    #     batch_size=4,
-    # )
-    # model = TripletModel(Builder)
-    # problem = TripletCapture(model, config)
-    # mobo = BDSIMMoBO(problem, "MOBO_Triplet1_15_20_4")
-    # mobo.optimise()
-    # mobo.plot_pareto()
+    config = OptConfig(
+        objectives=["T", "A", "D"],
+        constraints = {"T_min": 0.04, "D_max": 0.04},
+        bounds={
+            "d1": (0.0, 0.25),
+            "d2": (0.0, 0.25),
+            "d3": (0.0, 0.25),
+            "q1_L": (0.02, 0.05),
+            "q2_L": (0.02, 0.05),
+            "q3_L": (0.02, 0.05),
+            "aper1": (0.01, 0.04),
+            "aper2": (0.01, 0.04),
+            "aper3": (0.01, 0.04),
+        },
+        n_initial=15,
+        n_iter=20,
+        batch_size=4,
+    )
+    model = TripletModel(Builder)
+    problem = TripletCapture(model, config)
+    mobo = BDSIMOpt(problem, "MOBO_Triplet1_15_20_4")
+    mobo.optimise()
+    mobo.plot_pareto()
 
     # config = OptConfig(
     #     objectives=["T", "A", "D"],
